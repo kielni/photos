89113c608badd182c82ed37bf8166c1f737f9f71
Kimberly Nicholls <1649528+kielni@users.noreply.github.com>
2024-09-06 20:29:41 -0700

    remove photo-button

--- photo-button/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../deploy-button
-pip install -r requirements.txt -t .
-zip -r ../lambda.zip .
-echo
-echo "uploading"
-aws lambda update-function-code --function-name button-mms --zip-file fileb://../lambda.zip --region us-east-1 --profile megapis
-cd ../photo-button
